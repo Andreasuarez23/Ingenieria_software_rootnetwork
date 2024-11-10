9e5410c9670e3f09d6f516874dcdce37860ebc39
yohana <cpoteljunot@hotmail.com>
2024-11-10 18:18:11 -0300

    Error de directivas en daoefpublishingUser.

--- dao_library/entity_framework/publishing/DAOEFPublishingUser.cs
+++ dao_library/entity_framework/publishing/DAOEFPublishingUser.cs
@@ -1,3 +1,4 @@
+using dao_library;   
 using dao_library.Interfaces.publishing;
 using entities_library.publishing;
 using Microsoft.EntityFrameworkCore;
@@ -16,29 +17,30 @@ public class DAOEFPublishingUser : IDAOPublishingUser
 
     public async Task<IEnumerable<PublishingUser>> GetAll()
     {
-        return await _context.Set<PublishingUser>().ToListAsync();
+        return await context.Set<PublishingUser>().ToListAsync();
     }
 
 
     public async Task<PublishingUser> GetById(long id)
     {
-        return await _context.Set<PublishingUser>().FindAsync(id);
+        return await context.Set<PublishingUser>().FindAsync(id);
     }
 
     public async Task<PublishingUser> Save(PublishingUser publishingUser)
     {
-        // Si el ID de la publicación es 0, significa que es una nueva publicación
         if (publishingUser.Id == 0)
         {
-            await _context.Set<PublishingUser>().AddAsync(publishingUser);
+            await context.Set<PublishingUser>().AddAsync(publishingUser);
         }
         else
         {
-            _context.Set<PublishingUser>().Update(publishingUser);
+            context.Set<PublishingUser>().Update(publishingUser);
         }
         
-        await _context.SaveChangesAsync(); 
+        await context.SaveChangesAsync();
+        return publishingUser; 
     }
+
 
 
 }
