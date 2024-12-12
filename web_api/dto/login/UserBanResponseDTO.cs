a911942206127cc8112593890e016a6a688b0f4f
Mijaliv <mijal.nunez@itscordoba.edu.ar>
2024-12-12 12:55:55 -0300

    Creo response de userban

---
+++ web_api/dto/login/UserBanResponseDTO.cs
@@ -0,0 +1,12 @@
+using web_api.dto.common;
+
+namespace web_api.dto.login;
+
+public class UserBanResponseDTO : ResponsetDTO
+{
+    public long Id { get; set; }
+    public DateTime? StartDateTime { get; set; }
+    public DateTime? EndDateTime { get; set; }
+    public string? Reason { get; set; }
+    public long UserId { get; set; } // id usuario baneado.
+}
