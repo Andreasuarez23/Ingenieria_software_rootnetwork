b2b2a664261c1b56a02c160879d5c7d01b58a5b9
lorena <lorena.ramos@itscordoba.edu.ar>
2025-01-14 19:57:31 -0300

    actualiza

---
+++ dao_library/Migrations/20250114214349_Actualizando25.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace daolibrary.Migrations
+{
+    /// <inheritdoc />
+    public partial class Actualizando25 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Posts_PublishingId",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "PostId",
+                table: "Comments");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PublishingId",
+                table: "Comments",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<string>(
+                name: "User",
+                table: "Comments",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Posts_PublishingId",
+                table: "Comments",
+                column: "PublishingId",
+                principalTable: "Posts",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Posts_PublishingId",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "User",
+                table: "Comments");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PublishingId",
+                table: "Comments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PostId",
+                table: "Comments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Posts_PublishingId",
+                table: "Comments",
+                column: "PublishingId",
+                principalTable: "Posts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
